c0197c581fa3071d76141d81b96e3bbd112a3efd
jack <jack@jack-XPS-13-9343>
2018-04-07 18:51:29 +0800

    Change-Id: I7dbc6bab308fdc129846c62222e756387f9b6641 Signed-off-by: jack <jack@jack-XPS-13-9343>

--- src/crawler/HTTPUtils.java
+++ src/crawler/HTTPUtils.java
@@ -11,13 +11,9 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 
-/**
- * @author:合肥工业大学 管理学院 钱洋
- * @email:1563178220@qq.com
- * @ 
- */
 public class HTTPUtils {
-    //这个方法是向后台请求数据，获取html或者json等
+  
+          //这个方法是向后台请求数据，获取html或者json等
     public static String  getRawHtml(String personalUrl) throws InterruptedException,IOException {
         URL url = new URL(personalUrl);
         URLConnection conn = url.openConnection();
@@ -26,12 +22,11 @@ public class HTTPUtils {
             conn.setConnectTimeout(3000);
             in = conn.getInputStream();
         } catch (Exception e) {
-        }
-        //将获取的数据转化为String
+                    }
         String html = convertStreamToString(in);
         return html;
     }
-    //这个方法是将InputStream转化为String
+          //这个方法是将InputStream转化为String
     public static String convertStreamToString(InputStream is) throws IOException {
         if (is == null)
             return "";
@@ -41,7 +36,7 @@ public class HTTPUtils {
         try {
             while ((line = reader.readLine()) != null) {
                 sb.append(line);
-            }
+                            }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
